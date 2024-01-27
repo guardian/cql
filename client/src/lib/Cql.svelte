09700a263a105a44dbb369e1ac08cacd927132f3
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2024-01-27 22:39:52 +0000

    Add input handling for menu

--- client/src/lib/Cql.svelte
+++ client/src/lib/Cql.svelte
@@ -4,28 +4,89 @@
 	import Token from './Token.svelte';
 	import { onMount, afterUpdate } from 'svelte';
 
+	const TYPEAHEAD_TOKENS = ['QUERY_META_KEY', 'QUERY_META_VALUE'];
+
 	const exampleQuery = 'an (example AND query) +tag:tags-are-magic';
 
 	onMount(async () => {
-		console.log('mounted');
+    fetchLanguageServer(exampleQuery);
 	});
 
+	let overlayOffset = 0;
 	let cqlQuery = exampleQuery;
 	let ast = '';
 	let tokenisedChars = [];
-	let overlayOffset = 0;
-  let cursorOffset = 0;
-  let typeaheadOffset = 0;
-  let cursorMarker;
+	let typeaheadTokens = [];
+	let typeaheadOffsetChars = undefined;
+  let typeaheadOffsetPx = 0;
+	let inputElement;
+	let cursorMarker;
+  let menuIndex = 0;
+  let menuItems = [{ key: "One", value: "one"}, {key: "Two", value: "two"}, {key: "Three", value: "three"}];
 
 	const handleInput = (e: InputEvent) => {
-    cursorOffset = e.target.selectionStart;
-    fetchLanguageServer(e.target.value);
+		applyTypeahead(inputElement.selectionStart);
+		fetchLanguageServer(e.target.value);
+	};
+
+  const handleKeydown = (e) => {
+    if (typeaheadOffsetChars === undefined) {
+      // Only active when menu is active
+      return;
+    }
+
+    let handled = true;
+
+    switch(e.key) {
+      case "ArrowUp":
+        if (menuIndex > 0) menuIndex--;
+        break;
+        case "ArrowDown":
+        if (menuIndex < menuItems.length - 1) menuIndex++;
+        break;
+      case "Enter":
+        console.log("I'm done!")
+        break;
+      case "Escape":
+        console.log("I want out!");
+        break;
+      default:
+        handled = false;
+    }
+
+    if (handled) {
+      e.preventDefault();
+    }
   }
 
-  afterUpdate(() => {
-    typeaheadOffset = cursorMarker?.getBoundingClientRect().left;
-  });
+
+
+	const watchSelectionChange = () => {
+		document.addEventListener('selectionchange', handleSelection);
+	};
+
+	const unwatchSelectionChange = () => {
+		document.removeEventListener('selectionchange', handleSelection);
+	};
+
+	const handleSelection = (e: InputEvent) => {
+		applyTypeahead(inputElement.selectionStart);
+	};
+
+	const applyTypeahead = (cursorOffset: number) => {
+		const firstValidTypeaheadToken = typeaheadTokens.find(
+			(token) => cursorOffset >= token.start && cursorOffset <= token.end
+		);
+		if (firstValidTypeaheadToken) {
+			typeaheadOffsetChars = firstValidTypeaheadToken.start;
+		} else {
+			typeaheadOffsetChars = undefined;
+		}
+	};
+
+	afterUpdate(() => {
+		typeaheadOffsetPx = cursorMarker?.getBoundingClientRect().left;
+	});
 
 	const fetchLanguageServer = async (query: string) => {
 		cqlQuery = query;
@@ -34,6 +95,7 @@
 		const request = await fetch(`http://localhost:5050/cql?${urlParams}`);
 
 		ast = await request.json();
+		typeaheadTokens = ast.tokens.filter((token) => TYPEAHEAD_TOKENS.includes(token.tokenType));
 
 		tokenisedChars = addTokensToString(cqlQuery, ast.tokens);
 	};
@@ -50,47 +112,68 @@
 	const syncScrollState = (e: ScrollEvent) => {
 		overlayOffset = e.target.scrollLeft;
 	};
-
-	fetchLanguageServer(exampleQuery);
 </script>
 
 <div class="Cql__input-container">
-	<input class="Cql__input" value={cqlQuery} on:input={handleInput} on:scroll={syncScrollState} />
+	<input
+		class="Cql__input"
+		value={cqlQuery}
+		on:input={handleInput}
+    on:keydown={handleKeydown}
+		on:focus={watchSelectionChange}
+		on:blur={unwatchSelectionChange}
+		on:scroll={syncScrollState}
+		bind:this={inputElement}
+	/>
 	<div class="Cql__overlay-container">
-		<div class="Cql__overlay" contenteditable="true"  style="left: -{overlayOffset}px">
+		<div class="Cql__overlay" contenteditable="true" style="left: -calc({overlayOffset}px - 1em)">
 			{#each tokenisedChars || [] as { char, token }, index}
-				<Token str={char} {token} />{#if index === cursorOffset - 1}<span class="Cql__cursor-marker" bind:this={cursorMarker}></span>{/if}
+				<Token str={char} {token} />{#if index === typeaheadOffsetChars - 1}<span
+						class="Cql__cursor-marker"
+						bind:this={cursorMarker}
+					></span>{/if}
 			{/each}
 		</div>
 	</div>
-  <div class="Cql__typeahead" style="left: calc({typeaheadOffset}-1em)px"></div>
-  {#if ast.queryResult}<div class="Cql__output">{ast.queryResult}</div>{/if}
-  {#if ast.error}<div class="Cql__error">{ast.error}</div>{/if}
+	{#if typeaheadOffsetChars !== undefined}<div
+			class="Cql__typeahead"
+			style="left: {typeaheadOffsetPx - 7}px"
+		>
+    <ul>
+      {#each menuItems as { key, value }, index}
+      <li class:--selected={menuIndex === index}>
+        {key}
+      </li>
+      {/each}
+    </ul>
+  </div>{/if}
+	{#if ast.queryResult}<div class="Cql__output">{ast.queryResult}</div>{/if}
+	{#if ast.error}<div class="Cql__error">{ast.error}</div>{/if}
 </div>
 <div class="debug"><pre>{JSON.stringify(ast, null, '\t')}</pre></div>
 
 <style>
-  :root {
-    --input-width: 400px;
-  }
+	:root {
+		--input-width: 400px;
+	}
 
 	.Cql__input-container {
 		position: relative;
 	}
 
-  .Cql__overlay-container {
-    position: absolute;
+	.Cql__overlay-container {
+		position: absolute;
 		pointer-events: none;
 		top: 7px;
 		left: 7px;
 		width: var(--input-width);
-    overflow: hidden;
+		overflow: hidden;
 		white-space: nowrap;
-  }
+	}
 
-  .Cql__overlay {
-    position: relative;
-  }
+	.Cql__overlay {
+		position: relative;
+	}
 
 	.Cql__input {
 		width: var(--input-width);
@@ -116,22 +199,38 @@
 		white-space: pre;
 	}
 
-  .Cql__output, .Cql__error {
-    margin-top: 1rem;
+	.Cql__output,
+	.Cql__error {
+		margin-top: 1rem;
+	}
+
+	.Cql__error {
+		color: darkred;
+	}
+
+	.Cql__cursor-marker {
+		display: inline;
+		white-space: pre;
+	}
+	.Cql__typeahead {
+		position: absolute;
+		border: 2px solid grey;
+		width: 100px;
+    background-color: white;
+	}
+
+  .Cql__typeahead ul {
+    list-style: none;
+    padding: initial;
+    margin: initial;
+  }
+  .Cql__typeahead li {
+    list-style: none;
+    padding: 5px;
+    margin: initial;
   }
 
-  .Cql__error {
-    color: darkred;
-  }
-
-  .Cql__cursor-marker {
-    display: inline;
-    white-space: pre;
-  }
-  .Cql__typeahead {
-    position: absolute;
-    border: 2px solid grey;
-    width: 100px;
-    height: 300px;
+  .Cql__typeahead li.--selected {
+    background-color: #ddd;
   }
 </style>
