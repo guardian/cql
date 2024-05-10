49ca3127ef27868f03deb3da64bc32e0214cc6e6
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2024-05-10 17:09:54 +0100

    Use GuApiLambda

--- cdk/lib/cql-lambda.ts
+++ cdk/lib/cql-lambda.ts
@@ -1,6 +1,6 @@
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
-import { GuLambdaFunction } from '@guardian/cdk/lib/constructs/lambda';
+import { GuApiLambda } from '@guardian/cdk/lib/patterns/api-lambda';
 import type { App } from 'aws-cdk-lib';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
 
@@ -8,12 +8,17 @@ export class CqlLambda extends GuStack {
 	constructor(scope: App, id: string, props: GuStackProps) {
 		super(scope, id, props);
 
-		new GuLambdaFunction(this, 'cql-lambda', {
+		new GuApiLambda(this, 'cql-lambda', {
 			app: 'cql-lambda',
 			handler: 'cql.Handler::handleRequest',
 			functionName: `cql-lambda-${this.stage}`,
-      runtime: Runtime.JAVA_11,
-      fileName: "cql-lambda.jar"
+			runtime: Runtime.JAVA_11,
+			fileName: 'cql-lambda.jar',
+			api: {
+				id: 'query',
+				description: 'Get a CAPI query string from a CQL query',
+			},
+			monitoringConfiguration: { noMonitoring: true },
 		});
 	}
 }
