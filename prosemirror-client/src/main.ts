0fe4cda4f2bf938d4a7786e21cea2a45f39da2f4
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2024-11-26 10:14:41 +0000

    Minor typo

--- prosemirror-client/src/main.ts
+++ prosemirror-client/src/main.ts
@@ -18,7 +18,7 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   </div>
   <cql-input initial-value="" id="cql-input" popover-container-id="popover-container"></cql-input>
   <p>Press <tt>+</tt> to select a specific field to search.</p>
-  <p>Join search terms with <tt class="CqlToken__AND">OR</tt> and <tt class="CqlToken__AND">AND.</tt> Consecutive search terms, e.g. <tt class="CqlToken__STRING">this that</tt>, are implicitly joined with <tt class="CqlToken__OR">OR</tt>.</p>
+  <p>Join search terms with <tt class="CqlToken__AND">OR</tt> and <tt class="CqlToken__AND">AND</tt>. Consecutive search terms, e.g. <tt class="CqlToken__STRING">this that</tt>, are implicitly joined with <tt class="CqlToken__OR">OR</tt>.</p>
   <p>Group expressions with parenthesis, e.g. <tt class="CqlToken__STRING">one <tt class="CqlToken__LEFT_BRACKET">(</tt>two <tt class="CqlToken__AND">AND</tt> three<tt class="CqlToken__RIGHT_BRACKET">)</tt></tt>
   <div id="cql-sandbox" class="CqlSandbox">
     <div class="CqlSandbox__query-results">
