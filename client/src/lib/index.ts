4b2d5aa83dfe1df5538298759384473f51e4dbf5
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2024-12-05 16:22:05 +0000

    Remove old codemirror and svelte folders

--- client/src/lib/index.ts
+++
@@ -1,5 +0,0 @@
-// Reexport your entry components here
-import Cql from "./Cql.svelte";
-
-export const c = Cql
-
--- codemirror-client/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { CqlInput } from "./CqlInput";
-import "./style.css";
-
-customElements.define("cql-input", CqlInput);
-
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <cql-input />
-`;
