4e0b52bac76df360e951408c8698b4c0aa94762d
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2025-01-02 13:50:10 +0000

    Add library bundle to build
    Mainly to satisfy my curiosity re: bundle size, which turns out to be ~90kb gzipped

---
+++ client/src/lib.ts
@@ -0,0 +1,4 @@
+export { createCqlInput } from "./cqlInput/CqlInput";
+export { CqlClientService } from "./services/CqlService";
+export { TypeaheadHelpersCapi } from "./typeahead/typeaheadHelpersCapi";
+export { TypeaheadField } from "./lang/typeahead.ts";
---
+++ client/vite.lib.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  define: {
+    "process.platform": '"web"',
+  },
+
+  build: {
+    minify: true,
+    lib: {
+      entry: "src/lib.ts",
+      name: "Cql",
+      // the proper extensions will be added
+      fileName: "cql",
+      formats: ["es"],
+    },
+  },
+});
