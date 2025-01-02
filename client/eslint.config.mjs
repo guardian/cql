3dd61655f2794d7a723bc9a49a2c9183eef2440f
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2025-01-02 15:40:46 +0000

    ~destroy~fix the linter

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -3,17 +3,24 @@ import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
+  {
+    ignores: ["dist/*"],
+  },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   eslintConfigPrettier,
   {
     languageOptions: {
       parserOptions: {
-        projectService: true,
+        projectService: {
+          allowDefaultProject: [
+            "eslint.config.mjs",
+            "vite.config.ts",
+            "vite.lib.config.ts",
+            "happydom.ts",
+          ],
+        },
       },
     },
-  },
-  {
-    ignores: ["dist/*"],
   },
 ];
