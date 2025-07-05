996d61a0159c754888c62ea0c186f909dbf92d51
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2025-07-05 12:30:47 +0100

    More concise scanner tests

--- lib/cql/src/lang/scanner.spec.ts
+++ lib/cql/src/lang/scanner.spec.ts
@@ -1,108 +1,81 @@
 import { describe, expect, it } from "bun:test";
-import { Scanner } from "./scanner";
+import { Scanner, ScannerSettings } from "./scanner";
 import { eofToken, quotedStringToken, unquotedStringToken } from "./testUtils";
 import { Token, TokenType } from "./token";
 
 describe("scanner", () => {
+  const assertTokens = (query: string, expectedTokens: Token[], settings?: Partial<ScannerSettings>) => {
+    const scanner = new Scanner(query, settings);
+    const tokens = scanner.scanTokens();
+    expect(tokens).toEqual(expectedTokens);
+  }
+
   describe("unquoted strings", () => {
     it("should parse plain strings", () => {
-      const scanner = new Scanner("sausages");
-      const tokens = scanner.scanTokens();
-      const expectedTokens = [unquotedStringToken("sausages"), eofToken(8)];
-      expect(tokens).toEqual(expectedTokens);
+      assertTokens("sausages", [unquotedStringToken("sausages"), eofToken(8)])
     });
 
     it("should preserve whitespace at the end of the document", () => {
-      const scanner = new Scanner("magnificent octopus ");
-      const tokens = scanner.scanTokens();
-      const expectedTokens = [
+      assertTokens("magnificent octopus ", [
         unquotedStringToken("magnificent"),
         unquotedStringToken("octopus ", 12),
         eofToken(20),
-      ];
-      expect(tokens).toEqual(expectedTokens);
+      ])
     });
 
     it("should preserve whitespace at the end of string tokens", () => {
-      const scanner = new Scanner("a  +tag");
-      const tokens = scanner.scanTokens();
-      const expectedTokens = [
+      assertTokens("a  +tag", [
         unquotedStringToken("a "),
         new Token(TokenType.CHIP_KEY_POSITIVE, "+tag", "tag", 3, 6),
         eofToken(7),
-      ];
-      expect(tokens).toEqual(expectedTokens);
+      ])
     });
 
     it("should interpret strings that start with reserved words as strings", () => {
-      const scanner = new Scanner("ORCOMBE");
-      const tokens = scanner.scanTokens();
-      const expectedTokens = [unquotedStringToken("ORCOMBE"), eofToken(7)];
-      expect(tokens).toEqual(expectedTokens);
+      assertTokens("ORCOMBE", [unquotedStringToken("ORCOMBE"), eofToken(7)])
     });
   });
 
   describe("quoted strings", () => {
     it("should parse plain strings", () => {
-      const scanner = new Scanner('"sausages"');
-      const tokens = scanner.scanTokens();
-      const expectedTokens = [quotedStringToken("sausages"), eofToken(10)];
-      expect(tokens).toEqual(expectedTokens);
+      assertTokens('"sausages"', [quotedStringToken("sausages"), eofToken(10)])
     });
 
     it("should give a single token for strings separated with a space", () => {
-      const scanner = new Scanner('"magnificent octopus"');
-      const tokens = scanner.scanTokens();
-      const expectedTokens = [
+      assertTokens('"magnificent octopus"', [
         quotedStringToken("magnificent octopus"),
         eofToken(21),
-      ];
-
-      expect(tokens).toEqual(expectedTokens);
+      ])
     });
   });
 
   describe("fields", () => {
     it("should tokenise keys for fields", () => {
-      const scanner = new Scanner("+tag:");
-      const tokens = scanner.scanTokens();
-      const expectedTokens = [
+      assertTokens("+tag:", [
         new Token(TokenType.CHIP_KEY_POSITIVE, "+tag", "tag", 0, 3),
         new Token(TokenType.CHIP_VALUE, ":", undefined, 4, 4),
         eofToken(5),
-      ];
-
-      expect(tokens).toEqual(expectedTokens);
+      ])
     });
 
     it("should tokenise keys for fields with reversed polarity", () => {
-      const scanner = new Scanner("-tag:");
-      const tokens = scanner.scanTokens();
-      const expectedTokens = [
+      assertTokens("-tag:", [
         new Token(TokenType.CHIP_KEY_NEGATIVE, "-tag", "tag", 0, 3),
         new Token(TokenType.CHIP_VALUE, ":", undefined, 4, 4),
-        eofToken(5),
-      ];
-
-      expect(tokens).toEqual(expectedTokens);
+        eofToken(5)
+      ])
     });
 
     it("should tokenise key value pairs for fields", () => {
-      const scanner = new Scanner("+tag:tone/news");
-      const tokens = scanner.scanTokens();
-      const expectedTokens = [
+      assertTokens("+tag:tone/news", [
         new Token(TokenType.CHIP_KEY_POSITIVE, "+tag", "tag", 0, 3),
         new Token(TokenType.CHIP_VALUE, ":tone/news", "tone/news", 4, 13),
         eofToken(14),
-      ];
-
-      expect(tokens).toEqual(expectedTokens);
+      ])
     });
 
     it("should tokenise key value pairs for fields - 2", () => {
-      const scanner = new Scanner("+section:commentisfree");
-      const tokens = scanner.scanTokens();
-      const expectedTokens = [
+      assertTokens("+section:commentisfree", [
         new Token(TokenType.CHIP_KEY_POSITIVE, "+section", "section", 0, 7),
         new Token(
           TokenType.CHIP_VALUE,
@@ -112,74 +85,52 @@ describe("scanner", () => {
           21,
         ),
         eofToken(22),
-      ];
-
-      expect(tokens).toEqual(expectedTokens);
+      ])
     });
 
     it("should tokenise key value pairs for fields when the key value is in quotes", () => {
-      const scanner = new Scanner('+tag:"tone/news"');
-      const tokens = scanner.scanTokens();
-      const expectedTokens = [
+      assertTokens('+tag:"tone/news"', [
         new Token(TokenType.CHIP_KEY_POSITIVE, "+tag", "tag", 0, 3),
         new Token(TokenType.CHIP_VALUE, ':"tone/news"', "tone/news", 4, 15),
         eofToken(16),
-      ];
-
-      expect(tokens).toEqual(expectedTokens);
+      ])
     });
 
     it("should tokenise key value pairs for fields, and give `undefined` for empty quotes", () => {
-      const scanner = new Scanner('+tag:""');
-      const tokens = scanner.scanTokens();
-      const expectedTokens = [
+      assertTokens('+tag:""', [
         new Token(TokenType.CHIP_KEY_POSITIVE, "+tag", "tag", 0, 3),
         new Token(TokenType.CHIP_VALUE, ':""', undefined, 4, 6),
         eofToken(7),
-      ];
-
-      expect(tokens).toEqual(expectedTokens);
+      ])
     });
 
     it("should tokenise key value pairs for fields when the key value contains non-word characters", () => {
-      const scanner = new Scanner('+@tag:"tone/news"');
-      const tokens = scanner.scanTokens();
-      const expectedTokens = [
+      assertTokens('+@tag:"tone/news"', [
         new Token(TokenType.CHIP_KEY_POSITIVE, "+@tag", "@tag", 0, 4),
         new Token(TokenType.CHIP_VALUE, ':"tone/news"', "tone/news", 5, 16),
         eofToken(17),
-      ];
-
-      expect(tokens).toEqual(expectedTokens);
+      ])
     });
 
     it("should yield a query field key when a search key is incomplete", () => {
-      const scanner = new Scanner("example +");
-      const tokens = scanner.scanTokens();
-      const expectedTokens = [
+      assertTokens("example +", [
         unquotedStringToken("example"),
         new Token(TokenType.CHIP_KEY_POSITIVE, "+", undefined, 8, 8),
         eofToken(9),
-      ];
-      expect(tokens).toEqual(expectedTokens);
+      ])
     });
 
     it("should yield a query field value token when a query meta value is incomplete", () => {
-      const scanner = new Scanner("example +tag:");
-      const tokens = scanner.scanTokens();
-      const expectedTokens = [
+      assertTokens("example +tag:", [
         unquotedStringToken("example"),
         new Token(TokenType.CHIP_KEY_POSITIVE, "+tag", "tag", 8, 11),
         new Token(TokenType.CHIP_VALUE, ":", undefined, 12, 12),
         eofToken(13),
-      ];
-      expect(tokens).toEqual(expectedTokens);
+      ])
     });
 
     it("should tokenise groups and boolean operators - 1", () => {
-      const scanner = new Scanner("one AND (two OR three)");
-      const tokens = scanner.scanTokens();
-      const expectedTokens = [
+      assertTokens("one AND (two OR three)", [
         unquotedStringToken("one"),
         new Token(TokenType.AND, "AND", undefined, 4, 6),
         new Token(TokenType.LEFT_BRACKET, "(", undefined, 8, 8),
@@ -188,60 +139,46 @@ describe("scanner", () => {
         unquotedStringToken("three", 16),
         new Token(TokenType.RIGHT_BRACKET, ")", undefined, 21, 21),
         eofToken(22),
-      ];
-      expect(tokens).toEqual(expectedTokens);
+      ])
     });
 
     it("should tokenise groups and boolean operators - 2", () => {
-      const scanner = new Scanner("one AND two");
-      const tokens = scanner.scanTokens();
-      const expectedTokens = [
+      assertTokens("one AND two", [
         unquotedStringToken("one"),
         new Token(TokenType.AND, "AND", undefined, 4, 6),
         unquotedStringToken("two", 8),
         eofToken(11),
-      ];
-
-      expect(tokens).toEqual(expectedTokens);
+      ])
     });
 
     it("should tokenise groups", () => {
-      const scanner = new Scanner("(two OR three)");
-      const tokens = scanner.scanTokens();
-      const expectedTokens = [
+      assertTokens("(two OR three)", [
         new Token(TokenType.LEFT_BRACKET, "(", undefined, 0, 0),
         unquotedStringToken("two", 1),
         new Token(TokenType.OR, "OR", undefined, 5, 6),
         unquotedStringToken("three", 8),
         new Token(TokenType.RIGHT_BRACKET, ")", undefined, 13, 13),
         eofToken(14),
-      ];
-      expect(tokens).toEqual(expectedTokens);
+      ])
     });
   });
 
   describe("settings", () => {
     it("should disable groups, interpreting parentheses as strings", () => {
-      const scanner = new Scanner("(two)", { groups: false });
-      const tokens = scanner.scanTokens();
-      const expectedTokens = [
+      assertTokens("(two)", [
         unquotedStringToken("(two", 0),
         unquotedStringToken(")", 4),
         eofToken(5),
-      ];
-      expect(tokens).toEqual(expectedTokens);
+      ], { groups: false })
     });
 
     it("should disable operators, interpreting them as strings", () => {
-      const scanner = new Scanner("this AND that", { operators: false });
-      const tokens = scanner.scanTokens();
-      const expectedTokens = [
+      assertTokens("this AND that", [
         unquotedStringToken("this", 0),
         unquotedStringToken("AND", 5),
         unquotedStringToken("that", 9),
         eofToken(13),
-      ];
-      expect(tokens).toEqual(expectedTokens);
+      ], { operators: false });
     });
   });
 });
