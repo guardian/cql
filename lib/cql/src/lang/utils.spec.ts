d2ceb882c94f17cecc4cb7d73eeecf2f82761f30
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2025-08-26 22:17:07 +0100

    Format

--- lib/cql/src/lang/utils.spec.ts
+++ lib/cql/src/lang/utils.spec.ts
@@ -2,7 +2,7 @@ import { describe, expect, test } from "bun:test";
 import { unescapeQuotes } from "./utils";
 
 describe("language utils", () => {
-    test("unescapeStr", () => {
-        expect(unescapeQuotes(`\\"sausages\\"`)).toBe(`"sausages"`)
-    })
-})
+  test("unescapeStr", () => {
+    expect(unescapeQuotes(`\\"sausages\\"`)).toBe(`"sausages"`);
+  });
+});
